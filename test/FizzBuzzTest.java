a4e65044a3c906ffeda6d272f3cbe185c9f65373
anhnbt-it <anhnbt.it@gmail.com>
2020-10-17 15:03:38 +0700

    Add FizzBuzzTest.java

--- test/FizzBuzzTest.java
+++ test/FizzBuzzTest.java
@@ -1,10 +1,11 @@
+import static org.junit.jupiter.api.Assertions.*;
+
 /**
  * Created by IntelliJ IDEA.
  * User: AnhNBT (anhnbt.it@gmail.com)
  * Date: 10/17/2020
- * Time: 2:59 PM
+ * Time: 3:03 PM
  */
-public class FizzBuzz {
-    public FizzBuzz() {}
+class FizzBuzzTest {
 
 }
