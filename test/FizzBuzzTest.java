3b65fd1360d377da018c770286684e028f978912
anhnbt-it <anhnbt.it@gmail.com>
2020-10-17 15:08:24 +0700

    Add test 3 to fizz

--- test/FizzBuzzTest.java
+++ test/FizzBuzzTest.java
@@ -1,4 +1,7 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Created by IntelliJ IDEA.
@@ -7,5 +10,14 @@ import static org.junit.jupiter.api.Assertions.*;
  * Time: 3:03 PM
  */
 class FizzBuzzTest {
+
+    @Test
+    @DisplayName("3 to Fizz")
+    void testFizz() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String expected = "fizz";
+        String actual = fizzBuzz.translate(3);
+        assertEquals(expected, actual);
+    }
 
 }
